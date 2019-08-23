15205249b1620671b0e11950b0af9a1248832510
Alberto <alberto@MacBook-Air-di-Alberto.local>
2019-08-23 18:45:41 +0200

    Add Filter

--- js/index.js
+++ js/index.js
@@ -2,34 +2,38 @@ import getData from './getData';
 import Vue from 'vue/dist/vue.min.js';
 
 window.vueistance = new Vue({
-  el: '#app',
-  data: {
-    cards: [{chord_HTML: 'Please Wait'}],
-    recentCards: []
-  },
-  created() {
-    this.setCards();
-  },
-  methods: {
-    setCards(s = '') {
-      getData(s).then(data => {
-        if (data.length) {
-          return data;
-        } else {
-          return getData(this.recentCards[this.recentCards.length - 1].chord_ID);
+    el: '#app',
+    data: {
+        cards: [{ chord_HTML: 'Please Wait' }],
+        recentCards: []
+    },
+    created() {
+        this.setCards();
+    },
+    methods: {
+        setCards(s = '') {
+            getData(s)
+                .then(data => {
+                    if (data.length) {
+                        return data;
+                    } else {
+                        return getData(this.recentCards[0].chord_ID);
+                    }
+                })
+                .then(cards => {
+                    cards = cards.filter(card => {
+                        return card.probability > 0.1;
+                    });
+                    this.cards = cards;
+                });
+        },
+        selectCard(card = {}) {
+            this.recentCards.push(card);
+            this.cards = { chord_HTML: 'Please Wait' };
+            this.setCards(card.child_path);
+        },
+        upperCase(string = 'Please Wait') {
+            return string.toUpperCase();
         }
-      }).then(cards => {
-        cards = cards.slice(0, 10);
-        this.cards = cards;
-      });
-    },
-    selectCard(card = {}) {
-      this.recentCards.push(card);
-      this.cards = {chord_HTML: 'Please Wait'};
-      this.setCards(card.child_path);
-    },
-    upperCase(string = 'Please Wait') {
-      return string.toUpperCase();
     }
-  }
 });
